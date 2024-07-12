445721875aa5112dff015ce328364d448af34403
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-12 11:45:03 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# FTWindow
+# A ftwindow Flet app
+
+An example of a minimal Flet app.
+
+To run the app:
+
+```
+flet run [app_directory]
+```
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import flet as ft
 
-def main(page: ft.Page):
-    page.window.bgcolor = ft.colors.TRANSPARENT
-    page.bgcolor = ft.colors.TRANSPARENT
-    page.window.title_bar_hidden = True
-    page.window.frameless = True
-    page.window.left = 400
-    page.window.top = 200
-    page.add(ft.ElevatedButton("I'm a floating button!"))
 
-ft.app(target=main)
+def main(page: ft.Page):
+    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+
+
+ft.app(main)
