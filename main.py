e2ef7823a547486a695ae06c00035ebfdbaac66e
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-12 12:15:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 import flet as ft
+import flet_easy as fs
+app = fs.FletEasy(route_init="/flet-easy")
+# We add a page
+@app.page(route="/flet-easy", title="Flet-Easy")
+def index_page(data: fs.Datasy):
+
+    return ft.View(
+        controls=[
+            ft.Text("Home page"),
+            ft.FilledButton("Go to Counter", on_click=data.go("/counter")),
+        ],
+        vertical_alignment="center",
+        horizontal_alignment="center",
+    )
+# We add a second page
+@app.page(route="/counter", title="Counter")
+def counter_page(data: fs.Datasy):
+    page = data.page
+    txt_number = ft.TextField(value="0", text_align="right", width=100)
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+    return ft.View(
+        controls=[
+            ft.Row(
+                [
+                    ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                    txt_number,
+                    ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                ],
+                alignment="center",
+            ),
+            ft.FilledButton("Go to Home", on_click=data.go("/flet-easy")),
+        ],
+        vertical_alignment="center",
+        horizontal_alignment="center",
+    )
 
 
-def main(page: ft.Page):
-    page.window.bgcolor = ft.colors.TRANSPARENT
-    page.bgcolor = ft.colors.TRANSPARENT
-    # page.window.title_bar_hidden = True
-    # page.window.frameless = True
-    page.window.left = 200
-    page.window.top = 200
-    page.add(ft.ElevatedButton("I'm a floating button!"))
+# We run the application
 
-
-ft.app(target=main)
+app.run()
