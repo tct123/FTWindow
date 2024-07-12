485da69964a9ad97d6be576bd3c0319ee8b50203
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-12 12:16:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import flet as ft
 import flet_easy as fs
+
 app = fs.FletEasy(route_init="/flet-easy")
+
+
 # We add a page
 @app.page(route="/flet-easy", title="Flet-Easy")
 def index_page(data: fs.Datasy):
@@ -12,17 +15,22 @@ def index_page(data: fs.Datasy):
         vertical_alignment="center",
         horizontal_alignment="center",
     )
+
+
 # We add a second page
 @app.page(route="/counter", title="Counter")
 def counter_page(data: fs.Datasy):
     page = data.page
     txt_number = ft.TextField(value="0", text_align="right", width=100)
+
     def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
         page.update()
+
     def plus_click(e):
         txt_number.value = str(int(txt_number.value) + 1)
         page.update()
+
     return ft.View(
         controls=[
             ft.Row(
@@ -38,5 +46,7 @@ def counter_page(data: fs.Datasy):
         vertical_alignment="center",
         horizontal_alignment="center",
     )
+
+
 # We run the application
 app.run()
