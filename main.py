169082951b09ffdcb27340d9649fbfd70d226ed8
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-12 11:49:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main(page: ft.Page):
     page.bgcolor = ft.colors.TRANSPARENT
     page.window.title_bar_hidden = True
     page.window.frameless = True
-    page.window.left = 400
+    page.window.left = 200
     page.window.top = 200
     page.add(ft.ElevatedButton("I'm a floating button!"))
 
