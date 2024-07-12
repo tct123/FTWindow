874bcd078bd9994d66b690a5721efdc4c3c05b99
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-13 00:17:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ app = fs.FletEasy(route_init="/")
 # We add a page
 @app.page(route="/", title="FTWindow - Home")
 def index_page(data: fs.Datasy):
+    page = data.page
+    page.adaptive = True
     return ft.View(
         controls=[
+            ft.AppBar(title=ft.Text("FTWindow")),
             ft.Text("Home page"),
             ft.FilledButton("Go to Counter", on_click=data.go("/counter")),
         ],
@@ -21,7 +24,9 @@ def index_page(data: fs.Datasy):
 @app.page(route="/counter", title="Counter")
 def counter_page(data: fs.Datasy):
     page = data.page
+    page.adaptive = True
     txt_number = ft.TextField(value="0", text_align="right", width=100)
+    page.appbar = ft.AppBar(title=ft.Text("FTWindow"))
 
     def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
@@ -33,6 +38,7 @@ def counter_page(data: fs.Datasy):
 
     return ft.View(
         controls=[
+            ft.AppBar(title=ft.Text("FTWindow")),
             ft.Row(
                 [
                     ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
@@ -45,6 +51,17 @@ def counter_page(data: fs.Datasy):
         ],
         vertical_alignment="center",
         horizontal_alignment="center",
+    )
+
+
+@app.page_404("/FletEasy-404", title="Error 404", page_clear=True)
+def page404(data: fs.Datasy):
+    return ft.View(
+        controls=[
+            ft.Text(f"Custom 404 error", size=30),
+        ],
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
     )
 
 
