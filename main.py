914a1195b530ddbc3140ab72ce8e675c67f2dc75
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-12 11:46:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import flet as ft
 
-
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.window.bgcolor = ft.colors.TRANSPARENT
+    page.bgcolor = ft.colors.TRANSPARENT
+    page.window.title_bar_hidden = True
+    page.window.frameless = True
+    page.window.left = 400
+    page.window.top = 200
+    page.add(ft.ElevatedButton("I'm a floating button!"))
 
-
-ft.app(main)
+ft.app(target=main)
